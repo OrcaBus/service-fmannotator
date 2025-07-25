1856d9eef7ffd1c78733a40c128a5bf5f2034149
mmalenic <mmalenic1@gmail.com>
2025-07-25 15:37:17 +1000

    fix: unmarshall SQS event and process all records

--- app/cmd/portalrunid/main.go
+++ app/cmd/portalrunid/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	fmannotator "github.com/OrcaBus/service-fmannotator/app"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
+	"log/slog"
 )
 
 var (
@@ -11,13 +15,30 @@ var (
 )
 
 // Handler for the portalRunId annotator function.
-func Handler(event fmannotator.Event) error {
+func Handler(event events.SQSEvent) error {
 	config, token, err := fmannotator.LoadCachedConfig(secretCache)
 	if err != nil {
 		return err
 	}
 
-	return fmannotator.PortalRunId(event, config, token)
+	for _, record := range event.Records {
+		body := record.Body
+		slog.Debug(fmt.Sprintf("processing record: %v", body))
+
+		var workflowRunEvent fmannotator.Event
+		err = json.Unmarshal([]byte(body), &workflowRunEvent)
+		if err != nil {
+			return err
+		}
+		slog.Debug(fmt.Sprintf("unmarshalled event: %v", workflowRunEvent))
+
+		err = fmannotator.PortalRunId(workflowRunEvent, config, token)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func main() {
